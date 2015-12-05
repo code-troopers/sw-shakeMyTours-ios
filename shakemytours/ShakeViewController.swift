e94aa4e3772b38b8186c2bb20ede176d04cade59
Cedric Gatay <cedric@gatay.fr>
2015-12-05 01:44:52 +0100

    chore(poc) : added 5 items on screen
    Not beautiful, but working

--- shakemytours/ShakeViewController.swift
+++ shakemytours/ShakeViewController.swift
@@ -12,11 +12,17 @@ import UIKit
 class ShakeViewController : UIViewController{
     @IBOutlet weak var firstItem: ShakeItem!
     @IBOutlet weak var secondItem: ShakeItem!
+    @IBOutlet weak var thirdItem: ShakeItem!
+    @IBOutlet weak var fourthItem: ShakeItem!
+    @IBOutlet weak var fifthItem: ShakeItem!
     override func viewDidLoad() {
         self.navigationController?.navigationBar.hidden = false
-        if let loaded = LoaderService().loadLocalData(2){
+        if let loaded = LoaderService().loadLocalData(5){
             self.firstItem.updateView(loaded[0])
             self.secondItem.updateView(loaded[1])
+            self.thirdItem.updateView(loaded[2])
+            self.fourthItem.updateView(loaded[3])
+            self.fifthItem.updateView(loaded[4])
         }
     }
     
